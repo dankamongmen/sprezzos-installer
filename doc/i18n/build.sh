ff537553e5e98c36387523ad0113697a78ea2003
Joey Hess <joeyh@debian.org>
2005-03-17 18:38:01 +0000

    tagging version 20050317
    r26312

---
+++ doc/i18n/build.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+xsltproc=`which xsltproc`
+lynx=`which lynx`
+sgmltools=`which sgmltools`
+w3m=`which w3m`
+stylesheet=/usr/share/sgml/docbook/stylesheet/xsl/nwalsh/html/chunk.xsl
+
+if [ -n "$xsltproc" ] ; then
+    if [ -e "$stylesheet" ]; then
+	$xsltproc style-html.xsl i18n.xml
+    else
+	echo stylesheet missing. Please install the docbook-xsl Debian package
+	exit 1
+    fi
+else
+    echo xsltproc not found. Please install the xsltproc Debian package
+    exit 1
+fi
+
+if [ -f index.html ] ; then
+ mv index.html i18n.html
+ if [ -n "$sgmltools" -a -n "$w3m" ] ; then
+    # To be checked
+    $sgmltools --backend=txt i18n.html >i18n.txt
+ else
+    if [ -n "$lynx" ] ; then
+	$lynx -dump -nolist i18n.html >i18n.txt
+    else
+	echo sgmltools, w3m or lynx not found. 
+        echo You need installing either sgmltools+w3m or lynx for
+        echo being able to convert the HTML documentation to text
+	exit 1
+    fi
+ fi
+fi
+
+
+
---
+++ doc/manual/build/build.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+if [ -z "$languages" ]; then
+	# Please add languages only if they build properly.
+	# languages="en cs es fr ja nl pt_BR" # ca da de el eu it ru
+
+	# Buildlist of languages to be included on RC3 CD's
+	languages="en cs de es fr ja pt_BR ru"
+fi
+
+if [ -z "$architectures" ]; then
+	architectures="alpha arm hppa i386 ia64 m68k mips mipsel powerpc s390 sparc"
+fi
+
+if [ -z "$destination" ]; then
+	destination="/tmp/manual"
+fi
+
+if [ -z "$formats" ]; then
+        #formats="html pdf ps txt"
+        formats="html pdf txt"
+fi
+
+[ -e "$destination" ] || mkdir -p "$destination"
+
+if [ "$official_build" ]; then
+	# Propagate this to children.
+	export official_build
+fi
+
+for lang in $languages; do
+    echo "Language: $lang";
+    for arch in $architectures; do
+	echo "Architecture: $arch"
+	if [ -n "$noarchdir" ]; then
+		destsuffix="$lang"
+	else
+		destsuffix="${lang}.${arch}"
+	fi
+	./buildone.sh "$arch" "$lang" "$formats"
+	mkdir -p "$destination/$destsuffix"
+	for format in $formats; do
+	    if [ "$format" = html ]; then
+		mv ./build.out/html/*.html "$destination/$destsuffix"
+	    else
+		mv ./build.out/install.$lang.$format "$destination/$destsuffix"
+	    fi
+	done
+	./clear.sh
+    done
+done
+
+PRESEED="../en/appendix/example-preseed.xml"
+if [ -f $PRESEED ] && [ -f preseed.awk ] ; then
+    gawk -f preseed.awk $PRESEED >$destination/example-preseed.txt
+fi
